7ef3fea60c80f2344b517e347c0597c8537137be
monymachado <monymachad@yahoo.com.br>
2020-02-11 04:02:11 -0300

    Commit final do primeiro capítulo

--- README.md
+++ README.md
@@ -1,38 +1,58 @@
 <b> #Roteiro JS
-<ul> <li>
-Capítulo 1 </li> 
-Alert </br>
-Variáveis </br>
-Console.log </br>
-Array e dicionário </br>
-Condicional (If else) </br>
-Laços de repetição while e for </br>
+<ul> 
+
+> Capítulo 1 
+<li>
+Alert e Console.log </li> 
+##MÉTODOS DE CONVERSÃO DE STRING </br>
+ String (Frases): Replace, lowerCase, upperCase </br>
 Push, Pop e length </br>
+##Array e dicionário </br>
 Reverse array </br>
 toString para array </br>
 Join para array </br>
-Variavel global / local </br>
-String (Frases): Replace 
-lowerCase, upperCase </br>
-Redirecionar para outra página </br>
+Condicional (If else) </br>
+Laços de repetição: while e for </br>
 Data </br>
 Function </br>
-Dom </br>
+##Variáveis: Variavel global / local </br>
+##Eventos - Manipulando a página: </br>
 getElement</br>
-InnerHTML</br>
 onclick</br>
-server</br>
+Dom: document </br>
+InnerHTML</br>
+
+##Redirecionar para outra página:</br>
+window.open</br> 
+window.open</br>
 onChange</br>
 onLoad</br>
 onmouseover</br>
+server</br>
 </ul>
-#Configuração </br>
-1>> Extensão de edição do README no VSCode
- <ul> <li> <b>Auto-Open Markdown Preview</b> </li> </ul> - Uma vez instalada a extensão, mostrará um Preview no próprio VSCode (ao lado direito), de como ficará o resultado final do README.md no navagador.
 
-2>> Extensão para acompanhar as alterações no GIT:
- <ul> <li> <b> GIT HISTORY  </b> </li></ul> 
-- Depois de instalado, abrir a paleta de comandos F1 e executar o comando “git: View File History”; </br>
+#Configuração </br>
+
+## 1 Extensão de edição do README no VSCode
+ <ul> <li> <b>
+
+ > Auto-Open Markdown Preview</b> </li> 
+ - Uma vez instalada a extensão, mostrará um Preview no próprio VSCode (ao lado direito), de como ficará o resultado final do README.md no navagador.
+</ul>
+
+## 2 Extensão para acompanhar as alterações no GIT:
+ <ul> <li> <b>
+ 
+  >  GIT HISTORY  </b> </li>
+- Depois de instalado, abrir a paleta de comandos F1 e executar o comando “git: View File History”; </ul> 
+
+> Prettier
+- Suporta: JS, Angular, GraphQL, Json, CSS, HTML e Markdown (entre outros)
+
+> Color HighLight: para cores
+
+> Tema
+- Dracula
 
 ------- 
 By Mony Machado 
--- js/main.js
+++ js/main.js
@@ -1,13 +1,167 @@
-/** CONDICIONAIS, LAÇOS DE REPETIÇÕES E DATES */
+/** FUNÇÃO */
+/** Web - Manipulando Elementos da página: onclick */
+/** FUNÇÃO */
+function clicou(){
+    //Usa a função innerHTML, para inserir um texto
+    //A função innerHTML permite tmb colocar HTML, texto
+    document.getElementById("agradecimento").innerHTML = "<i>Obrigada por clicar..</i>";
+    //console.log(document.getElementById("Agradecimento"));
+    //alert("Obrigada por clicar..");
+}
+function redirecionar(){
+    //Abre uma página em outra aba (outra janela)
+    window.open("https://github.com/monymachado");
+    //Abre uma pag. da mesma ABA
+    //window.location.href = "https://github.com/monymachado";
+}
 
-/** ARRAYS */
+function trocar(elemento){
+    elemento.innerHTML = "Obrigada por passar o mouse";
+    document.getElementById("mousemove").style.color = "red";
+    //document.getElementById("mousemove").innerHTML = "Obrigada por passar o mouse";
+    //alert("Trocar texto...");
+}
+    
+function voltar (elemento){
+    elemento.innerHTML = "Passe o mouse aqui para Voltar";
+    document.getElementById("mousemove").style.color = "black";
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui para Voltar";
+}
+
+function load(){
+    alert("Pagina carregada");
+}
+
+function funcChange(elemento) {
+    console.log(elemento.value);
+}
+/** FUNÇÃO 
+//Com variáveis fixas (o user não digita os valores)
+function soma(v1, v2){
+    return v1 + v2;
+    }
+
+function setReplace(frase, nome, novo_nome) {
+//Permite substituir uma sequência de caracteres por outra.
+    return frase.replace(nome, novo_nome);
+}
+alert(soma(5,10));
+alert(setReplace("Vai Japão", "Japão", "Brasil"));
+//Var global
+var valida=0;    
+function validaIdade(idade) {
+    if (idade >= 18) {
+        valida=true;
+    } else {
+        valida=false;
+    }
+    return idade;
+}
+var idade = prompt("Qual a sua idade: ");
+validaIdade(idade);
+console.log(valida);
+*/
+
+/** Concatenar - Testando
+var v1, v2;
+v1 = "Simplicity is the ultimate sophistication.";
+v2 = "Leonardo da Vinci:";
+//console.log(var1 + var2 + "1425-1519");
+//alert(var1 + var2 + "1425,1519");
+//console.log(join(" - "));
+//console.log(var1 + var2 + "1425", "-", "1519");
+//console.log(var1 + var2 + "1425" "-" "1519");
+//console.log(var1 + var2 + "1425 + "-" + 1519");
+//console.log(var1 + var2 + 1425, "-", 1519);
+*/
+
+
+/** DATES
+var d = new Date();
+var diaName = new Array ("Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado");
+var mesName = new Array ("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro");
+document.write ("<h2> Hoje é " + diaName[d.getDay()] + ", " + d.getDate() + " de " + mesName[d.getMonth()] + " de " + d.getFullYear() + ". </h2>");
+alert("Hoje é " + diaName[d.getDay()] + ", " + d.getDate() + " de " + mesName[d.getMonth()] + " de " + d.getFullYear());
+ */
+
+/*alert(d.getMonth()+1);
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());*/
+
+/**ESTRUTURA/LAÇOS DE REPETIÇÕES [ FOR ] 
+// A var [ i ] tem o mesmo valor que count
+var i;
+for (i=0; i <=5; i++) {
+    alert(i);
+
+};
+*/
+
+/**ESTRUTURA/LAÇOS DE REPETIÇÕES [ WHILE ]
+var count = 0;
+while (count <= 5) {
+    console.log (count);
+    alert(count);
+    count++;
+}
+  */
+
+
+/** CONDICIONAIS
+
+var idade = prompt("Qual a sua idade? ");
+if (idade >= 18) {
+    alert("Maior de Idade! ");
+    } 
+    else {
+        alert("Menor de idade");
+    };
+    */
+
+/** Lista de DiDIONÁRIO 
+//Objeto Json
+var dicioFrutas = [{nome:"Maçã", cor:"Vermelha"}, {nome:"Uva", cor:"Lilás"}]
+console.log(dicioFrutas);
+alert(dicioFrutas[1].nome, dicioFrutas[1].cor);
+*/
+
+/** DICIONÁRIO 
+var dicio = {nome:"Mação", cor:"Vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
 
 
 
+/** ARRAYS 
+var lista = ["Banana", "Maçã", "Goiaba"]
+//Coloca um item a mais na lista
+lista.push("Uva");
+//Retira o último item da lista
+//lista.pop();
+//Imprime a fruta correspondete ao índice 0
+//console.log("Fruta eh:", lista);
+//Elementos ao contrário da lista
+//console.log(lista.reverse());
+//Tamanho / qtd de elementos da lista
+//console.log(lista.length);
+ 
+console.log(lista[0]);
+console.log(lista.toString()[0]);
+//Você consegue mudar caracteres e tmb transforma em string, 
+//igual o toString (que coloca virgula automaticamente)
+console.log(lista.join(" - "));
 
-/** Recebe uma variavel com o campo no qual vc irá digitar o seu nome */
-// Criação de Variáveis
+//Caixa de dialogos - iteração com o usuário que aparece na tela
+alert(lista);
+alert(lista.length);
+fim do ARRAY*/
+
+
+/** Recebe uma variavel com o campo no qual vc irá digitar o seu nome 
 var nome = prompt('Digite o seu nome');
+// Variáveis com valores fixos
 var n1 = 10;
 var n2 = 12;
 var frase = "China é o melhor país do mundo"
@@ -20,7 +174,8 @@ console.log(n1 * n2);
 // MANIPULANDO STRING/Frase
 //toLowerCase: Maiusculas
 console.log(frase.toLowerCase("China", "Brasil"));
-//Replaces: Trocas uma frase por por outra
+//Replaces: Troca uma frase por outra
 //console.log(frase.replace("China", "Brasil"));
 // ou o alert
 alert(frase.replace("China", "Brasil"));
+*/
